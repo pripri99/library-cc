10ad3952378ec3e73d62e1ec474dd3910f84957d
pripri99 <36493403+pripri99@users.noreply.github.com>
2023-03-25 00:42:18 -0400

    add html page and css page

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const producer = require("./producer");
 const consumer = require("./consumer");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("views"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
 producer("Hello, KafkaJS!");
