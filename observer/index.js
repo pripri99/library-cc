b2aae93fa818e6d7e66b5983a5308ad032ecc181
pripri99 <36493403+pripri99@users.noreply.github.com>
2023-04-03 01:35:17 -0400

    fix observer

--- observer/index.js
+++ observer/index.js
@@ -1,4 +1,5 @@
 const { Kafka } = require("kafkajs");
+const fs = require("fs").promises;
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
@@ -10,102 +11,55 @@ app.use(express.json());
 
 const kafka = new Kafka({
   clientId: "observer",
-  brokers: ["kafka:9092"], // Update this with your Kafka broker addresses
+  brokers: ["kafka:9092"],
 });
 
-const consumer = kafka.consumer({ groupId: "observer-group" });
 const producer = kafka.producer();
 
-const initKafkaConsumer = async () => {
-  let totalElapsedTime = 0;
-  let totalArrivalTime = 0;
-  let totalServiceTime = 0;
-  let numJobs = 0;
-  let numWorkers = 0;
-
+const getObserverResults = async (jobId) => {
+  const consumer = kafka.consumer({ groupId: "observer-group" });
   await consumer.connect();
-  await consumer.subscribe({ topic: "job-results", fromBeginning: true });
+  await consumer.subscribe({ topic: "observer-queue", fromBeginning: true });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const result = JSON.parse(message.value.toString());
-      console.log("Job result received:", result);
-
-      // Update the statistics
-      const elapsedTime = result.elapsedTime;
-      const arrivalTime = Date.now() - result.arrivalTime;
-      const serviceTime = elapsedTime - arrivalTime;
-      totalElapsedTime += elapsedTime;
-      totalArrivalTime += arrivalTime;
-      totalServiceTime += serviceTime;
-      numJobs++;
-      numWorkers = result.numWorkers;
-
-      // Calculate the arrival rate, service rate, and response time
-      const arrivalRate = numJobs / (totalArrivalTime / 1000);
-      const serviceRate = numJobs / (totalServiceTime / 1000);
-      const responseTime = totalElapsedTime / numJobs;
-
-      // Determine the worker action based on the rates
-      let workerAction = "nothing";
-      if (numWorkers < numJobs && arrivalRate > serviceRate) {
-        workerAction = "add";
-      } else if (numWorkers > 1 && arrivalRate < serviceRate / 2) {
-        workerAction = "remove";
-      }
-
-      // Send the observer message to the Kafka topic
-      const observerMessage = {
-        jobId: result.jobId,
-        observerInfo: {
-          arrivalRate,
-          serviceRate,
-          responseTime,
-          workerAction,
-        },
-      };
-
-      await producer.send({
-        topic: "observer-queue",
-        messages: [{ value: JSON.stringify(observerMessage) }],
-      });
-
-      console.log("Observer message sent:", observerMessage);
-    },
+  return new Promise((resolve) => {
+    consumer.run({
+      eachMessage: async ({ message }) => {
+        const observerMessage = JSON.parse(message.value.toString());
+        if (observerMessage.jobId === jobId) {
+          const observerInfo = observerMessage.observerInfo;
+          const htmlPath = path.join(__dirname, "public", "observer.html");
+          const html = await fs.readFile(htmlPath, "utf-8");
+          const renderedHtml = html
+            .replace("{{jobId}}", observerMessage.jobId)
+            .replace("{{arrivalRate}}", observerInfo.arrivalRate.toFixed(2))
+            .replace("{{serviceRate}}", observerInfo.serviceRate.toFixed(2))
+            .replace("{{responseTime}}", observerInfo.responseTime.toFixed(2))
+            .replace("{{workerAction}}", observerInfo.workerAction);
+          resolve(renderedHtml);
+        }
+      },
+    });
   });
 };
 
-app.get("/observer", (req, res) => {
-  // Get the observer results from the Kafka topic
-  const consumer = kafka.consumer({ groupId: "observer-group" });
-  consumer.connect().then(() => {
-    consumer
-      .subscribe({ topic: "observer-queue", fromBeginning: true })
-      .then(() => {
-        consumer.run({
-          eachMessage: ({ message }) => {
-            const observerMessage = JSON.parse(message.value.toString());
-            const observerInfo = observerMessage.observerInfo;
-            const htmlPath = path.join(__dirname, "public", "observer.html");
-            fs.readFile(htmlPath, "utf-8").then((html) => {
-              const renderedHtml = html
-                .replace("{{jobId}}", observerMessage.jobId)
-                .replace("{{arrivalRate}}", observerInfo.arrivalRate.toFixed(2))
-                .replace("{{serviceRate}}", observerInfo.serviceRate.toFixed(2))
-                .replace(
-                  "{{responseTime}}",
-                  observerInfo.responseTime.toFixed(2)
-                )
-                .replace("{{workerAction}}", observerInfo.workerAction);
-              res.send(renderedHtml);
-            });
-          },
-        });
-      });
-  });
+app.get("/", (req, res) => {
+  const jobId = req.query.jobId;
+
+  if (!jobId) {
+    res.status(400).send("Missing jobId parameter");
+    return;
+  }
+
+  getObserverResults(jobId)
+    .then((html) => {
+      res.send(html);
+    })
+    .catch((err) => {
+      console.error("Error getting observer results:", err);
+      res.status(500).send("Error getting observer results");
+    });
 });
 
-app.listen(port, async () => {
+app.listen(port, () => {
   console.log(`Observer is running on port ${port}`);
-  await initKafkaConsumer();
 });
