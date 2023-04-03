28c20450864ac13913d4afd5604bc827ee430ca6
pripri99 <36493403+pripri99@users.noreply.github.com>
2023-04-03 01:22:38 -0400

    add observer

--- observer/index.js
+++ observer/index.js
@@ -1,8 +1,9 @@
 const { Kafka } = require("kafkajs");
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +17,12 @@ const consumer = kafka.consumer({ groupId: "observer-group" });
 const producer = kafka.producer();
 
 const initKafkaConsumer = async () => {
+  let totalElapsedTime = 0;
+  let totalArrivalTime = 0;
+  let totalServiceTime = 0;
+  let numJobs = 0;
+  let numWorkers = 0;
+
   await consumer.connect();
   await consumer.subscribe({ topic: "job-results", fromBeginning: true });
 
@@ -24,20 +31,37 @@ const initKafkaConsumer = async () => {
       const result = JSON.parse(message.value.toString());
       console.log("Job result received:", result);
 
-      // Perform the algorithm to determine the info
-      // Average rate of arrival
-      // Average rate of service
-      // Average response time
-      // Add/remove/nothing worker decision
+      // Update the statistics
+      const elapsedTime = result.elapsedTime;
+      const arrivalTime = Date.now() - result.arrivalTime;
+      const serviceTime = elapsedTime - arrivalTime;
+      totalElapsedTime += elapsedTime;
+      totalArrivalTime += arrivalTime;
+      totalServiceTime += serviceTime;
+      numJobs++;
+      numWorkers = result.numWorkers;
+
+      // Calculate the arrival rate, service rate, and response time
+      const arrivalRate = numJobs / (totalArrivalTime / 1000);
+      const serviceRate = numJobs / (totalServiceTime / 1000);
+      const responseTime = totalElapsedTime / numJobs;
+
+      // Determine the worker action based on the rates
+      let workerAction = "nothing";
+      if (numWorkers < numJobs && arrivalRate > serviceRate) {
+        workerAction = "add";
+      } else if (numWorkers > 1 && arrivalRate < serviceRate / 2) {
+        workerAction = "remove";
+      }
 
       // Send the observer message to the Kafka topic
       const observerMessage = {
         jobId: result.jobId,
         observerInfo: {
-          arrivalRate: 10, // Replace with the actual arrival rate
-          serviceRate: 8, // Replace with the actual service rate
-          responseTime: 2, // Replace with the actual response time
-          workerAction: "add", // Replace with the actual worker action
+          arrivalRate,
+          serviceRate,
+          responseTime,
+          workerAction,
         },
       };
 
@@ -52,7 +76,33 @@ const initKafkaConsumer = async () => {
 };
 
 app.get("/observer", (req, res) => {
-  // Implement the page to display the observer results in a table or a graph
+  // Get the observer results from the Kafka topic
+  const consumer = kafka.consumer({ groupId: "observer-group" });
+  consumer.connect().then(() => {
+    consumer
+      .subscribe({ topic: "observer-queue", fromBeginning: true })
+      .then(() => {
+        consumer.run({
+          eachMessage: ({ message }) => {
+            const observerMessage = JSON.parse(message.value.toString());
+            const observerInfo = observerMessage.observerInfo;
+            const htmlPath = path.join(__dirname, "public", "observer.html");
+            fs.readFile(htmlPath, "utf-8").then((html) => {
+              const renderedHtml = html
+                .replace("{{jobId}}", observerMessage.jobId)
+                .replace("{{arrivalRate}}", observerInfo.arrivalRate.toFixed(2))
+                .replace("{{serviceRate}}", observerInfo.serviceRate.toFixed(2))
+                .replace(
+                  "{{responseTime}}",
+                  observerInfo.responseTime.toFixed(2)
+                )
+                .replace("{{workerAction}}", observerInfo.workerAction);
+              res.send(renderedHtml);
+            });
+          },
+        });
+      });
+  });
 });
 
 app.listen(port, async () => {
