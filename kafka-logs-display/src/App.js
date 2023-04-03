808a1f714d61579b309e0d61d6516aa890f72611
pripri99 <36493403+pripri99@users.noreply.github.com>
2023-04-03 01:52:06 -0400

    organize directories

--- kafka-logs-display/src/App.js
+++ kafka-logs-display/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE_URL = "http://localhost:3000"; // Replace with your server's base URL
+const API_BASE_URL = "http://localhost:6000"; // Replace with your server's base URL
 
 function App() {
   const [topic, setTopic] = useState("addBook");
